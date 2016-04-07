269bfda9ab96467c781d60208cad58adbaa84789
Jose Alanya <jose.alanya@gmail.com>
2016-04-06 20:30:57 -0500

    Add new feature

---
+++ app/assets/frontend/main.jsx
@@ -0,0 +1,20 @@
+import Greet from './greet.jsx';
+
+class Main extends React.Component {
+  render() {
+    return (
+        <Greet />
+    );
+  }
+}
+
+let documentReady = () => {
+  ReactDOM.render(
+    <Main />,
+    document.getElementById('react')
+  );
+};
+
+$(documentReady);
+
+window.Main = Main;
