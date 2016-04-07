df1326729611099fd67fae91481a8fa4cbb1d324
Jose Alanya <jose.alanya@gmail.com>
2016-04-06 20:36:08 -0500

    Srta

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -11,7 +11,7 @@ class Main extends React.Component {
 let documentReady = () => {
   ReactDOM.render(
     <Main />,
-    document.getElementById('react')
+    document.getElementById('  react')
   );
 };
 
